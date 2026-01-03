93565e8c68ac20ee6a60e712d4844a15e88738d3
princev47 <most.lyprince2004@gmail.com>
2026-01-03 16:35:40 +0530

    llm

--- backend/controllers/aiConnect.js
+++ backend/controllers/aiConnect.js
@@ -15,7 +15,7 @@ export const analyzeReport = async (req, res) => {
     if (!report.originalText)
       return res.status(400).json({ message: "No extracted text found" });
 
-    const response = await axios.post('http://127.0.0.1:8000/summarize',{
+    const response = await axios.post("https://medical-report-summarizer-2.onrender.com/summarize",{
       report_text: report.originalText,
     });
 
